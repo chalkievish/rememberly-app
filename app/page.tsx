c6d2017304095ac9dafd4f47c7398740655be7a8
Vish Personal Laptop <vishaal.varma@outlook.com>
2023-09-09 18:06:27 +1000

    Added friend cards with dummy data

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,74 @@
 import { Button } from "@/components/ui/button"
-import { TypographyH1 } from "@/components/typography/TypographyH1"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { TypographyH1 } from "@/components/typography/typographyH1"
+import { MoreHorizontal } from "lucide-react"
+import FriendData from "@/public/friendData"
+
+const friendData = FriendData
+
+// Helper function to calculate the number of days between two dates
+function calculateDaysAgo(dateString: string): number {
+  const currentDate = new Date();
+  const lastMessagedDate = new Date(dateString);
+  const timeDifference = currentDate.getTime() - lastMessagedDate.getTime();
+  const daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  return daysAgo;
+}
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <TypographyH1>
-          Rememberly
-        </TypographyH1>
-      </div>
-      <div>
-        <Button>Add new note</Button>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div className="flex flex-col space-y-4">
+        <div className="flex flex-row justify-between mb-6" >
+          <div className="flex flex-col">
+            <TypographyH1>
+              Rememberly
+            </TypographyH1>
+            <br />
+            <p>
+              Have more engaging and rewarding conversations with your mates.
+            </p>
+          </div>
+          <Button>Add friend</Button>
+        </div>
+        <div className="flex grow grid grid-cols-3 gap-4">
+          {friendData.friends.map((friend, index) => (
+            <Card key={index}>
+              <CardHeader>
+                <div className="flex row justify-between">
+                  <Avatar>
+                    <AvatarImage src={friend.profile_picture_url} />
+                    <AvatarFallback>
+                      {friend.first_name[0]}{friend.last_name[0]}
+                    </AvatarFallback>
+                  </Avatar>
+                  <Button variant="ghost" size="icon">
+                    <MoreHorizontal className="h-4 w-4" />
+                  </Button>
+                </div>
+                <CardTitle>{friend.nickname}</CardTitle>
+                <CardDescription>Last spoke {calculateDaysAgo(friend.date_last_messaged)} days ago</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>
+                  <span className="font-medium">Topics:</span> {friend.upcoming_thing}
+                </p>
+                <p>
+                  <span className="font-medium">Next meet:</span> {friend.date_next_meet}
+                </p>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
       </div>
     </main>
   )
---
+++ public/friendData.js
@@ -0,0 +1,142 @@
+const FriendData = {
+    "friends": [
+        {
+            "first_name": "Alice",
+            "last_name": "Smith",
+            "nickname": "Ally",
+            "birth_date": "1990-05-15",
+            "profile_picture_url": "https://example.com/alice.jpg",
+            "relation_to_user": "College roommate",
+            "date_last_messaged": "2023-08-25",
+            "upcoming_thing": "Brunch on Sunday",
+            "inside_jokes": [
+                "Remember that road trip to Vegas?"
+            ],
+            "other_notes": "Loves hiking and cooking."
+        },
+        {
+            "first_name": "John",
+            "last_name": "Doe",
+            "nickname": "JD",
+            "birth_date": "1985-12-10",
+            "profile_picture_url": "https://example.com/john.jpg",
+            "relation_to_user": "Childhood friend",
+            "date_last_messaged": "2023-09-01",
+            "upcoming_thing": "Movie night this Friday",
+            "inside_jokes": [
+                "Our high school pranks"
+            ],
+            "other_notes": "Enjoys playing guitar."
+        },
+        {
+            "first_name": "Emily",
+            "last_name": "Johnson",
+            "nickname": "Em",
+            "birth_date": "1992-03-20",
+            "profile_picture_url": "https://example.com/emily.jpg",
+            "relation_to_user": "Work colleague",
+            "date_last_messaged": "2023-08-29",
+            "upcoming_thing": "Conference next month",
+            "inside_jokes": [],
+            "other_notes": "Loves sci-fi books."
+        },
+        {
+            "first_name": "David",
+            "last_name": "Brown",
+            "nickname": "Dave",
+            "birth_date": "1988-07-05",
+            "profile_picture_url": "https://example.com/david.jpg",
+            "relation_to_user": "Cousin",
+            "date_last_messaged": "2023-07-15",
+            "upcoming_thing": "Family reunion in December",
+            "inside_jokes": [
+                "Our camping trip last summer"
+            ],
+            "other_notes": "Fond of fishing."
+        },
+        {
+            "first_name": "Olivia",
+            "last_name": "Taylor",
+            "nickname": "Liv",
+            "birth_date": "1995-02-18",
+            "profile_picture_url": "https://example.com/olivia.jpg",
+            "relation_to_user": "High school friend",
+            "date_last_messaged": "2023-08-22",
+            "upcoming_thing": "Planning a road trip",
+            "inside_jokes": [
+                "Our prom night"
+            ],
+            "other_notes": "Loves photography."
+        },
+        {
+            "first_name": "Michael",
+            "last_name": "Williams",
+            "nickname": "Mike",
+            "birth_date": "1986-09-30",
+            "profile_picture_url": "https://example.com/michael.jpg",
+            "relation_to_user": "Neighbor",
+            "date_last_messaged": "2023-08-30",
+            "upcoming_thing": "Cookout this Saturday",
+            "inside_jokes": [
+                "Our gardening competition"
+            ],
+            "other_notes": "Enthusiastic about gardening."
+        },
+        {
+            "first_name": "Sophia",
+            "last_name": "Martinez",
+            "nickname": "Sophie",
+            "birth_date": "1993-06-12",
+            "profile_picture_url": "https://example.com/sophia.jpg",
+            "relation_to_user": "Sister's best friend",
+            "date_last_messaged": "2023-08-27",
+            "upcoming_thing": "Shopping spree next week",
+            "inside_jokes": [],
+            "other_notes": "Avid fashion enthusiast."
+        },
+        {
+            "first_name": "Daniel",
+            "last_name": "Anderson",
+            "nickname": "Dan",
+            "birth_date": "1991-11-03",
+            "profile_picture_url": "https://example.com/daniel.jpg",
+            "relation_to_user": "Gym buddy",
+            "date_last_messaged": "2023-08-28",
+            "upcoming_thing": "Fitness competition in two months",
+            "inside_jokes": [
+                "Our cheat day feasts"
+            ],
+            "other_notes": "Passionate about fitness."
+        },
+        {
+            "first_name": "Mia",
+            "last_name": "Garcia",
+            "nickname": "Mimi",
+            "birth_date": "1994-04-25",
+            "profile_picture_url": "https://example.com/mia.jpg",
+            "relation_to_user": "Cousin",
+            "date_last_messaged": "2023-08-26",
+            "upcoming_thing": "Planning a beach vacation",
+            "inside_jokes": [
+                "Our summer camp memories"
+            ],
+            "other_notes": "Loves surfing."
+        },
+        {
+            "first_name": "William",
+            "last_name": "Clark",
+            "nickname": "Will",
+            "birth_date": "1989-08-08",
+            "profile_picture_url": "https://example.com/william.jpg",
+            "relation_to_user": "Childhood friend",
+            "date_last_messaged": "2023-08-23",
+            "upcoming_thing": "Hiking trip next weekend",
+            "inside_jokes": [
+                "Our treehouse adventures"
+            ],
+            "other_notes": "Avid hiker and nature lover."
+        }
+    ]
+}
+
+export default FriendData;
